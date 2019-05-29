52a28c710e816cbb60118cf67c902395102d1043
tech-camp <>
2019-05-29 17:14:13 +0900

    indexを編集しました

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,11 @@
-@extends('layout')
+<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>Pictweet</title>
+    </head>
 
-@section('content')
-  <p>Hello World!</p>
-@endsection
+    <body>
+      <p>Hello World!</p>
+    </body>
+  </html>
