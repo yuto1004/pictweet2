9de657f1ded18e9334292f196151c803562ab1db
tech-camp <>
2019-05-30 12:01:59 +0900

    tweetクラスのDB作成

---
+++ database/migrations/2019_05_30_010512_create_tweets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTweetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('text');
+            $table->text('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tweets');
+    }
+}
