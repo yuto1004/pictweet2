1f25a1d7077ef7b28582b7c1019d66f6ccd7031e
tech-camp <>
2019-06-03 09:59:42 +0900

    削除機能

--- resources/views/tweets/index.blade.php
+++ resources/views/tweets/index.blade.php
@@ -4,6 +4,14 @@
 <div class="contents row">
   @foreach($tweets as $tweet)
     <div class="content_post" style="background-image: url({{ $tweet->image }});">
+      @if (Auth::check() && Auth::user()->id == $tweet->user_id)
+        <div class="more">
+          <span><img src="images/arrow_top.png"></span>
+          <ul class="more_list">
+            <li><a href="/tweets/{{$tweet->id}}/delete">削除</a></li>
+          </ul>
+        </div>
+      @endif
       <p>{{ $tweet->text }}</p>
       <span class="name"> 
         <a href="#">
